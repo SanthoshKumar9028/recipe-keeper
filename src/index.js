ad696d98e5d9d7e350f068e3da39a2c8eb28c214
SanthoshKumar9028 <71515865+SanthoshKumar9028@users.noreply.github.com>
2022-05-30 07:55:02 +0530

    Added basic JSX Elements

--- src/index.js
+++ src/index.js
@@ -5,17 +5,28 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const container = React.createElement(
-  "div",
-  null,
-  React.createElement("h1", { className: "title" }, "Hello world"),
-  React.createElement(
-    "ul",
-    null,
-    React.createElement("li", null, "item1"),
-    React.createElement("li", null, "item2"),
-    React.createElement("li", null, "item3")
-  )
+// const container = React.createElement(
+//   "div",
+//   null,
+//   React.createElement("h1", { className: "title" }, "Hello world"),
+//   React.createElement(
+//     "ul",
+//     null,
+//     React.createElement("li", null, "item1"),
+//     React.createElement("li", null, "item2"),
+//     React.createElement("li", null, "item3")
+//   )
+// );
+
+const container = (
+  <div>
+    <h1 className="title">Hello world</h1>
+    <ul>
+      <li>item1</li>
+      <li>item2</li>
+      <li>item3</li>
+    </ul>
+  </div>
 );
 
 root.render(container);
