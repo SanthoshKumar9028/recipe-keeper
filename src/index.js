6e13ec5d7347c43a37314287f1991a8fc189486f
SanthoshKumar9028 <71515865+SanthoshKumar9028@users.noreply.github.com>
2022-05-30 07:52:25 +0530

    Added basic react elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./index.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(null);
+
+const container = React.createElement(
+  "div",
+  null,
+  React.createElement("h1", { className: "title" }, "Hello world"),
+  React.createElement(
+    "ul",
+    null,
+    React.createElement("li", null, "item1"),
+    React.createElement("li", null, "item2"),
+    React.createElement("li", null, "item3")
+  )
+);
+
+root.render(container);
